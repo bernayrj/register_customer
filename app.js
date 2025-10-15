a2aae84fce5bf23defdec5b601c43dd93f714d47
unknown <bernayrj@hotmail.com>
2025-10-15 02:43:20 -0400

    update para expopagos bco plaza

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const { db } = require('./db/connection');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const { db } = require("./db/connection");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'form.html'));
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "form.html"));
 });
 
-app.post('/submit', async (req, res) => {
-    const { nombre, apellido, correo, empresa, contacto, cargo, telefonos } = req.body;
-    try {
-        const result = await db.query("INSERT INTO customer (customer_name, customer_lastname, customer_email, customer_enterprise, customer_person, customer_position, customer_phone) VALUES (?, ?, ?, ?, ?, ?, ?)", 
-            [nombre, apellido, correo, empresa, contacto, cargo, telefonos]);
-        res.json( { success: true } );
-    } catch (err) {
-        console.error(err);
-        return res.json({ success: false, error: 'Error al guardar en la base de datos.' });
-    }
+app.post("/submit", async (req, res) => {
+  const { nombre, apellido, correo, empresa, contacto, cargo, telefonos } =
+    req.body;
+  try {
+    const result = await db.query(
+      "INSERT INTO customer (customer_name, customer_lastname, customer_email, customer_enterprise, customer_person, customer_position, customer_phone, customer_event) VALUES (?, ?, ?, ?, ?, ?, ?, 'expopago_bcoplz')",
+      [nombre, apellido, correo, empresa, contacto, cargo, telefonos]
+    );
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    return res.json({
+      success: false,
+      error: "Error al guardar en la base de datos.",
+    });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en puerto ${PORT}`);
+  console.log(`Servidor escuchando en puerto ${PORT}`);
 });
